86e7aa9f7969faf5461883989d2486fd6fae3a2d
anshhvarma <anshvarma83691@gmail.com>
2024-06-24 21:27:45 +0530

    vercel

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ import TrackVisibility from 'react-on-screen';
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
-// import { HashLink } from "react-router-hash-link";
+import { HashLink } from "react-router-hash-link";
 
 
 export const Banner = () => {
