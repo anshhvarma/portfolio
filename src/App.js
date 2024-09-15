769746753e64330f39529325baa807c01c918b29
anshhvarma <anshvarma83691@gmail.com>
2024-09-15 13:44:28 +0530

    skills

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <>
       <NavBar/>
-       <Home />
+       <Home/>
     </>
 
     
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,30 +1,23 @@
-import { BallCanvas } from "./canvas/Ball.js"
+import { BallCanvas } from "./canvas/Ball.js";
 import 'react-multi-carousel/lib/styles.css';
-import {technologies} from "../constant/index.js"
-
+import { technologies } from "../constant/index.js";
 
 export const Skills = () => {
   return (
     <section className="skill" id="skills">
-        <div className="container">
-                <div className="skill-bx wow">
-                        <h2>Skills</h2>
-                        <p>I'm a skilled Full-Stack  Developer with experience in Java , and expertise in frameworks such as React, Nextjs etc. I'm a quick learner who can easily collaborate with clients and push myself to fulfill required expectation.</p>
-                    </div>
-                    <div className="balls-container" >
-                    {
-                        technologies.map((data, index)=>{
-                            return (
-                                <>
-                                <div className="ball">
-                                    <BallCanvas icon={data.icon} ></BallCanvas>
-                                </div>
-                                </>
-                            )
-                        })
-                    }
-                    </div>
+      <div className="container">
+        <div className="skill-bx wow">
+          <h2>Skills</h2>
+          <p>I'm a skilled Full-Stack Developer with experience in Java, and expertise in frameworks such as React, Next.js, etc. I'm a quick learner who can easily collaborate with clients and push myself to fulfill required expectations.</p>
         </div>
+        <div className="balls-container">
+          {technologies.map((data, index) => (
+            <div className="ball" key={index}>
+              <BallCanvas icon={data.icon} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
